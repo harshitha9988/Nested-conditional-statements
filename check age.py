b6c872faf9100b3e66bdadc6b7d0526a3fcf8de1
harshitha9988 <harshitha.uppala@outlook.com>
2025-03-23 16:37:36 -0400

    age program

---
+++ check age.py
@@ -0,0 +1,13 @@
+x=int(input("Please enter your age:   "))
+
+if x<10 :
+    print("you are not allowed to attend this class")
+
+if x>20:
+        print("you are not allowed to attend this class")
+
+
+if x<20 and x>10 :
+    print("you are allowed to attend this class")
+
+    
